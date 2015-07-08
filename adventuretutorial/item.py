be576582255bba15d61588e7686b7339d0cc7eac
Sebastian Khan <sebastian.khan@astro.cf.ac.uk>
2015-07-09 00:11:17 +0200

    added init and started items file

---
+++ adventuretutorial/item.py
@@ -0,0 +1,23 @@
+#!/opt/local/bin/python
+
+# package to contain all items
+
+class Item():
+    """The base class for all items"""
+    def __init__(self, name, description, value):
+        self.name = name
+        self.description = description
+        self.value = value
+
+    def  __str__(self):
+        return "{}\n=====\n{}\nValue: {}\n".format(
+            self.name, self.description, self.value)
+
+class Gold(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(name="Gold",
+                        description="A round coin with {} stamped on the front".format(
+                            str(self.amt)),
+                        value=self.amt)
+
