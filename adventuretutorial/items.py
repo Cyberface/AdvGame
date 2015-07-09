06f59f9925782530bba6f7e9855d9568c818a8a7
Sebastian Khan <sebastian.khan@astro.cf.ac.uk>
2015-07-09 08:43:29 +0200

    renamed item to items

--- adventuretutorial/items.py
+++ adventuretutorial/items.py
@@ -17,7 +17,9 @@ class Gold(Item):
     def __init__(self, amt):
         self.amt = amt
         super().__init__(name="Gold",
-                        description="A round coin with {} stamped on the front".format(
-                            str(self.amt)),
+                        description="A round coin with {} \
+						stamped on the front".format(str(self.amt)),
                         value=self.amt)
+
+
 
