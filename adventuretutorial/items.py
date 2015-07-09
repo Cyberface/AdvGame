15aa1dc2096a573fc9131df5b0a1f9a1b393ac35
Sebastian Khan <sebastian.khan@astro.cf.ac.uk>
2015-07-09 11:47:49 +0200

    extended item class to incorporate weapons

--- adventuretutorial/items.py
+++ adventuretutorial/items.py
@@ -21,4 +21,32 @@ class Gold(Item):
                         "{} stamped on the front".format(str(self.amt)),
                         value=self.amt)
 
-print(Gold(15))
+# print(Gold(15))
+
+
+# extending the Item class to incorporate Weapons
+
+class Weapon(Item):
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super(Weapon, self).__init__(name, description, value)
+
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(
+            self.name, self.value, self.damage)
+
+class Rock(Weapon):
+    def __init__(self):
+        super(Rock, self).__init__(name="Rock",
+                            description="A first-sized rock, " +
+                            "suitable for bludgeoning.",
+                            value=0,
+                            damage=5)
+
+class Dagger(Weapon):
+    def __init__(self):
+        super(Dagger, self).__init__(name="Dagger",
+                            description="A small dagger with some rust. " +
+                            "Somewhat more dangerous than a rock.",
+                            value=10,
+                            damage=10)
