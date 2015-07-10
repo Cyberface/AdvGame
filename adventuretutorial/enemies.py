baf382a0be2dbd2ab39f47cb6a538c1525f87644
Sebastian Khan <sebastian.khan@astro.cf.ac.uk>
2015-07-10 21:17:06 +0100

    added enemies and tiles

---
+++ adventuretutorial/enemies.py
@@ -0,0 +1,28 @@
+#!/opt/local/bin/python
+
+# package to contain all enemies
+
+class Enemy(object):
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+
+    def is_alive(self):
+        return self.hp > 0
+
+# don't need to include is_alive method in the
+# sub class because they get access to the methods
+# in the superclass
+
+class GiantSpider(Enemy):
+    def __init__(self):
+        super(GiantSpider, self).__init__(name="Giant Spider",
+                                            hp=10,
+                                        damage=2)
+
+class Ogre(Enemy):
+    def __init__(self):
+        super(Ogre, self).__init__(name="Ogre",
+                                     hp=30,
+                                 damage=15)
---
+++ adventuretutorial/tiles.py
@@ -0,0 +1,6 @@
+import items, enemies
+
+class MapTile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
